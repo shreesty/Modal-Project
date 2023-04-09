d96f66a368e3f2167aaacaed20dc578341c0211d
shreesty <shristi35dahal@gmail.com>
2023-04-09 09:14:01 +0545

    Adding js

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 let btn=document.getElementById('btn');
 let modelContainer=document.getElementById('model-container');
+let closeBtn=document.getElementById('close');
+
+
+
+btn.addEventListener('click',function()
+{
+modelContainer.style.display='block';
+
+
+})
+
+
+closeBtn.addEventListener('click',function()
+{
+modelContainer.style.display='none';
+
+
+})
+
+
+ window.addEventListener('click',function(e)
+ {
+    if(e.target===modelContainer)
+    {
+       modelContainer.style.display='none';
+
+    }
+
+
+ })
+
+
+
+ 
